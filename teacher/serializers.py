d8a4f03ecf9ddd331737709b26ca065c13ca24a5
Luiz Fernando <luiz-fernando@outlook.com>
2025-04-16 13:23:35 -0300

    :rocket: - Correção do cadastro de professores

--- teacher/serializers.py
+++ teacher/serializers.py
@@ -1,9 +1,21 @@
-from rest_framework.serializers import ModelSerializer
+from rest_framework import serializers
 
 from teacher.models import Teacher
+from user.serializers import UserSerializer
 
 
-class TeacherSerializer(ModelSerializer):
+class TeacherSerializer(serializers.ModelSerializer):
+    user = UserSerializer()
+
     class Meta:
         model = Teacher
-        fields = ['id', 'email', 'first_name', 'last_name', 'user_type']
+        fields = ['id', 'user']
+
+    @classmethod
+    def create(self, validated_data):
+        user_data = validated_data.pop('user')
+        user = UserSerializer().create(user_data)  # Cria o User
+        # Cria o Teacher
+        teacher = Teacher.objects.create(user=user, **validated_data)
+        return teacher
+
--- user/models.py
+++ user/models.py
@@ -12,7 +12,7 @@ class UserManager(BaseUserManager):
 
     def _create_user_object(self, email, password, **extra_fields):
         if not email:
-            raise ValueError("The given email must be set")
+            raise ValueError('The given email must be set')
         user_type = extra_fields.get('user_type')
         if not user_type or user_type not in ['T', 'S']:
             raise ValueError('The user type must be "T" or "S"')
@@ -22,9 +22,6 @@ class UserManager(BaseUserManager):
         return user
 
     def _create_user(self, email, password, **extra_fields):
-        """
-        Create and save a user with the given email, and password.
-        """
         user = self._create_user_object(email, password, **extra_fields)
         user.save(using=self._db)
         return user
@@ -35,86 +32,79 @@ class UserManager(BaseUserManager):
         await user.asave(using=self._db)
         return user
 
-    def create_user(self, email, password=None, **extra_fields):
-        extra_fields.setdefault("is_staff", False)
-        extra_fields.setdefault("is_superuser", False)
+    def create_user(self, email, password, **extra_fields):
+        extra_fields.setdefault('is_staff', False)
+        extra_fields.setdefault('is_superuser', False)
         return self._create_user(email, password, **extra_fields)
 
     create_user.alters_data = True
 
-    async def acreate_user(self, email, password=None, **extra_fields):
-        extra_fields.setdefault("is_staff", False)
-        extra_fields.setdefault("is_superuser", False)
+    async def acreate_user(self, email, password, **extra_fields):
+        extra_fields.setdefault('is_staff', False)
+        extra_fields.setdefault('is_superuser', False)
         return await self._acreate_user(email, password, **extra_fields)
 
     acreate_user.alters_data = True
 
-    def create_superuser(self, email, password=None, **extra_fields):
-        extra_fields.setdefault("is_staff", True)
-        extra_fields.setdefault("is_superuser", True)
+    def create_superuser(self, email, password, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
 
-        if extra_fields.get("is_staff") is not True:
-            raise ValueError("Superuser must have is_staff=True.")
-        if extra_fields.get("is_superuser") is not True:
-            raise ValueError("Superuser must have is_superuser=True.")
+        if extra_fields.get('is_staff') is not True:
+            raise ValueError('Superuser must have is_staff=True.')
+        if extra_fields.get('is_superuser') is not True:
+            raise ValueError('Superuser must have is_superuser=True.')
 
         return self._create_user(email, password, **extra_fields)
 
     create_superuser.alters_data = True
 
-    async def acreate_superuser(
-        self, email=None, password=None, **extra_fields
-    ):
-        extra_fields.setdefault("is_staff", True)
-        extra_fields.setdefault("is_superuser", True)
+    async def acreate_superuser(self, email, password, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
 
-        if extra_fields.get("is_staff") is not True:
-            raise ValueError("Superuser must have is_staff=True.")
-        if extra_fields.get("is_superuser") is not True:
-            raise ValueError("Superuser must have is_superuser=True.")
+        if extra_fields.get('is_staff') is not True:
+            raise ValueError('Superuser must have is_staff=True.')
+        if extra_fields.get('is_superuser') is not True:
+            raise ValueError('Superuser must have is_superuser=True.')
 
         return await self._acreate_user(email, password, **extra_fields)
 
 
 class User(AbstractBaseUser, PermissionsMixin):
-
-    USER_TYPE_CHOICES = (
-        ('T', 'Professor'),
-        ('S', 'Estudante')
-    )
-
-    first_name = models.CharField(_("first name"), max_length=150)
-    last_name = models.CharField(_("last name"), max_length=150)
-    email = models.EmailField(_("email address"), unique=True)
+    USER_TYPE_CHOICES = (('T', 'Professor'), ('S', 'Estudante'))
+    first_name = models.CharField(_('first name'), max_length=150)
+    last_name = models.CharField(_('last name'), max_length=150)
+    email = models.EmailField(_('email address'), unique=True)
     user_type = models.CharField(
         choices=USER_TYPE_CHOICES, null=False, blank=False
     )
     is_staff = models.BooleanField(
-        _("staff status"),
+        _('staff status'),
         default=False,
         help_text=_(
-            "Designates whether the user can log into this admin site."
+            'Designates whether the user can log into this admin site.'
         ),
     )
     is_active = models.BooleanField(
-        _("active"),
+        _('active'),
         default=True,
         help_text=_(
-            "Designates whether this user should be treated as active. "
-            "Unselect this instead of deleting accounts."
+            'Designates whether this user should be treated as active. '
+            'Unselect this instead of deleting accounts.'
         ),
     )
-    date_joined = models.DateTimeField(_("date joined"), default=timezone.now)
+    date_joined = models.DateTimeField(_('date joined'), default=timezone.now)
 
     objects = UserManager()
 
-    EMAIL_FIELD = "email"
-    USERNAME_FIELD = "email"
+    EMAIL_FIELD = 'email'
+    USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = []
 
     class Meta:
-        verbose_name = _("user")
-        verbose_name_plural = _("users")
+        verbose_name = _('user')
+        verbose_name_plural = _('users')
 
     def clean(self):
         super().clean()
