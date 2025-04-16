eebe2bc2868256ebb28beada0e68689229203944
Luiz Fernando <luiz-fernando@outlook.com>
2025-04-16 13:23:35 -0300

    :rocket: - Correção do cadastro de professores

--- teacher/models.py
+++ teacher/models.py
@@ -1,6 +1,9 @@
+from django.db import models
+
 from user.models import User
 
 
-class Teacher(User):
-    teacher = False
-    ...
+class Teacher(models.Model):
+    user = models.OneToOneField(
+        User, on_delete=models.CASCADE, related_name='teacher'
+    )
