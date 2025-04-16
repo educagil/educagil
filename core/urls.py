beb771f12f040adcd2a9d99b050bdf5c1ebfb358
Luiz Fernando <luiz-fernando@outlook.com>
2025-04-16 15:06:42 -0300

    :rocket: - Configurado Swagger

--- core/urls.py
+++ core/urls.py
@@ -1,7 +1,19 @@
 from django.contrib import admin
 from django.urls import include, path
+from drf_yasg import openapi
+from drf_yasg.views import get_schema_view
+
+schema_view = get_schema_view(
+    openapi.Info(title='API', default_version='v1'),
+    public=True,
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('teacher/', include('teacher.urls'))
+    path(
+        'api/docs/',
+        schema_view.with_ui('swagger', cache_timeout=0),
+        name='swagger-ui',
+    ),
+    path('teacher/', include('teacher.urls')),
 ]
--- user/serializers.py
+++ user/serializers.py
@@ -7,7 +7,12 @@ class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = [
-            'id', 'first_name', 'last_name', 'email', 'user_type', 'password'
+            'id',
+            'first_name',
+            'last_name',
+            'email',
+            'user_type',
+            'password',
         ]
         extra_kwargs = {'password': {'write_only': True}}
 
