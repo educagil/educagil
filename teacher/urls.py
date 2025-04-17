31c3d6228eaf76e844e5cc6efab8f30a6a5af963
Luiz Fernando <luiz-fernando@outlook.com>
2025-04-17 19:01:01 -0300

    :rocket: - Criado teste para criação de usuário

--- teacher/urls.py
+++ teacher/urls.py
@@ -4,9 +4,14 @@ from rest_framework.routers import DefaultRouter
 from teacher.viewsets import TeacherViewSet
 
 router = DefaultRouter()
-router.register(r'teachers', TeacherViewSet)
+router.register(r'', TeacherViewSet, basename='teacher')
 
 
 urlpatterns = [
-    path('', include(router.urls)),
+    # path('', include(router.urls)),
+    path(
+        'create_user/',
+        TeacherViewSet.as_view({'post': 'create'}),
+        name='teacher_create_user'
+    )
 ]
